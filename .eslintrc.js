47d6297ad5544a44aa419ef863a7a60a3c32bb7e
Oppossome <dsecutler@gmail.com>
2023-11-22 21:46:31 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,7 +23,6 @@ module.exports = {
 		},
 	],
 	rules: {
-		"linebreak-style": "off",
 		"prettier/prettier": "error",
 		"@typescript-eslint/no-explicit-any": "off",
 		"@typescript-eslint/ban-types": "off",

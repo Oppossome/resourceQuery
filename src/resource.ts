4fae5e52ac605b13892e30acb9f8e41f51253d71
Oppossome <dsecutler@gmail.com>
2024-03-05 18:44:55 -0500

    Refresh

--- src/resource.ts
+++ src/resource.ts
@@ -1,15 +1,5 @@
 import { z } from "zod"
 
-/**
- * A symbol used to store metadata on a given resource class.
- */
-export const METADATA_KEY = Symbol("resource_metadata_key")
-
-/**
- * A type that returns the metadata of a given resource class.
- */
-export type MetadataOf<T extends { [METADATA_KEY]?: any }> = T[typeof METADATA_KEY]
-
 export class Resource {
 	public static extend<This extends typeof Resource, Schema extends z.ZodRawShape>(
 		this: This,

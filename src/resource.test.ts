a82855a0b057d39fd6335095874da5ec34c30f71
Oppossome <dsecutler@gmail.com>
2024-03-06 19:31:40 -0500

    Update resource.test.ts

--- src/resource.test.ts
+++ src/resource.test.ts
@@ -17,28 +17,21 @@ describe("Resource", () => {
 	}
 
 	it("should be possible to extend a resource", () => {
-		const message = new Message({
-			name: "John Doe",
-			content: "Hello, world!",
-		})
-
+		const message = new Message({ name: "John Doe", content: "Hello, world!" })
 		expect(message.message).toBe("John Doe: Hello, world!")
 	})
 
 	it("should return an existing resource if the uniqueId is the same", () => {
-		const firstMessage = new Message({
-			name: "John Doe",
-			content: "Hello, world!",
-		})
-
+		const firstMessage = new Message({ name: "John Doe", content: "Hello, world!" })
 		expect(firstMessage.message).toBe("John Doe: Hello, world!")
 
-		const secondMessage = new Message({
-			name: "John Doe",
-			content: "Hello, foo!",
-		})
-
+		const secondMessage = new Message({ name: "John Doe", content: "Hello, foo!" })
 		expect(firstMessage.message).toBe("John Doe: Hello, foo!")
 		expect(firstMessage).toBe(secondMessage)
+	})
+
+	it("should stringify without exposing any internal properties", () => {
+		const message = new Message({ name: "John Doe", content: "Hello, world!" })
+		expect(JSON.stringify(message)).toBe('{"name":"John Doe","content":"Hello, world!"}')
 	})
 })
